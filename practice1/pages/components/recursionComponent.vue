fc0a734350cf9ddfdd1ed2a643770a69777b8a7d
zhengkaiwen1 <2293138099@qq.com>
2021-03-16 06:38:32 +0800

    新增

---
+++ practice1/pages/components/recursionComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <recursionComponent></recursionComponent>
+       <div>
+           recursionComponent
+       </div>
+    </div>
+</template>
+<script>
+    export default{
+        name:"recursionComponent",
+        props:{
+            obj:{
+                type:Object,
+                default(){
+                    return {}
+                }
+            }
+        },
+        mounted(){
+            
+        }
+    }
+</script>
+<style>
+    
+</style>
--- practice1/pages/props/children.vue
+++ practice1/pages/props/children.vue
@@ -3,14 +3,17 @@
         <children1 :obj="obj" type="124" calss="test">
 
         </children1>
+        <recursionComponent></recursionComponent>
     </div>
 </template>
 <script>
     import children1 from "./children1.vue"
+    import recursionComponent from "@/components/recursionComponent.vue"
     // 如何把obj 传个obj2 呢
     export default{
         components:{
-            children1
+            children1,
+            recursionComponent
         },
         data(){
             return {
