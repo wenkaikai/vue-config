cacc498db9145688fc46fe91b001f1f45e830e2c
zhengkaiwen <1140073222@qq.com>
2021-03-16 09:03:01 +0800

    $on

---
+++ practice1/pages/$on/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <p @click='emit'>{{msg}}</p>
+        <p @click='emitOther'>{{msg2}}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'demo',
+    data () {
+        return {
+           msg : '点击后派发事件',
+           msg2 : '点击后派发事件2',
+        }
+    },
+    created () {
+  
+        this.$on(['wash_Goods','drive_Car'],(arg)=> {
+            console.log('真多事')
+        })
+        this.$on('wash_Goods',(arg)=> {
+            console.log(arg)
+        })
+        this.$on('drive_Car',(...arg)=> {
+            console.log(arg)
+            let [BMW,Ferrari] = arg
+            console.log(BMW,Ferrari)
+        })
+    },
+    methods : {
+        emit () {
+           this.$emit('wash_Goods','fish')
+        },
+        emitOther () {
+           this.$emit('drive_Car',['BMW','Ferrari'])
+        }
+    }
+  }
+  </script>
